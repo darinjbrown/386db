c1022c5fa0141c8cad94ed83fb0a8a1922d2a9ff
darinjbrown <browndar@sonoma.edu>
2019-02-14 08:44:30 -0800

    class changes

---
+++ api/app/Controllers/CourseController.js
@@ -0,0 +1,78 @@
+const dbConnection = require('../../database/mySQLconnect');
+const dateFormat = require('dateformat');
+
+function now() {
+    return dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss");
+}
+
+let count = 0;
+
+class CourseController {
+    constructor() {
+        console.log('Constructor of CourseController is called.');
+    }
+
+    async allGECoursesForTerm(ctx) {
+        return new Promise((resolve, reject) => {
+            const query = `
+                       SELECT cb.term             term,
+                               cb.subject          subject,
+                               cb.catalog          catalog,
+                               cb.course_title     course_title,
+	                       cb.department       department,
+                               cb.units
+                        FROM 
+                            course_base cb
+                        WHERE 
+	                    cb.ge_designation IS NOT NULL AND
+                            cb.term = ? 
+	                ORDER BY cb.term, cb.subject, cb.catalog, cb.units
+                        `;
+            dbConnection.query({
+                sql: query,
+                values: [ctx.params.term, ctx.params.subject]
+            }, (error, tuples) => {
+                if (error) {
+                    console.log("Connection error in CourseController::allCourses", error);
+                    ctx.body = [];
+                    ctx.status = 200;
+                    return reject(error);
+                }
+                ctx.body = tuples;
+                ctx.status = 200;
+                return resolve();
+            });
+        }).catch(err => console.log("Database connection error.", err));
+    }
+
+
+
+    async allCoursesBySubject(ctx) {
+        return new Promise((resolve, reject) => {
+            console.log()
+            const subject = ctx.params.subject;
+            const catalog = ctx.params.catalog;
+            let query = `SELECT * FROM course_catalog WHERE subject = ? AND catalog = ?`;
+            console.log(query);
+            dbConnection.query({
+                sql: query,
+                values: [subject]
+            }, (error, tuples) => {
+                if (error) {
+                    return reject(`Error when trying to retrieve course descriptions for ${subject} --- 
+                                        in courseDescriptionsForSubject. The error msg: ${error}`);
+                }
+                ctx.body = tuples;
+                ctx.status = 200;
+                return resolve();
+            })
+        }).catch(err => {
+            console.log(`Error in courseDescriptionsForSubject for subject ${subject}: ${err}`);
+            ctx.body = {error: err};
+            ctx.status = "Failed";
+        });
+    }
+}
+
+
+module.exports = CourseController
